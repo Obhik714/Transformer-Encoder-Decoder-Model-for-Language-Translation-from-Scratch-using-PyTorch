0c784f59c497fb9a29519a0530394e516daab059
Obhik <obhik714@gmail.com>
2026-07-29 23:39:48 +0530

    minor fix

--- src/modeling/architecture.py
+++ src/modeling/architecture.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import dataset
 import numpy as np
-import matplotlib.pyplot as plt
 import math
 
 class MultiHeadAttention(nn.Module):
